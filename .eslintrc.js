d5c0e557231adafab632a0a74bab52a3fa37b535
Nicolas Casado <nicolas@commonsclinic.com>
2023-04-01 09:52:22 -0600

    fixes eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,7 @@ module.exports = {
   env: {
     node: true,
   },
-  extends: ['plugin:vue/essential', 'eslint:recommended', 'plugin:prettier/recommended', 'prettier'],
+  extends: ['plugin:vue/essential', 'eslint:recommended', 'plugin:prettier/recommended'],
   parserOptions: {
     ecmaVersion: 2020,
   },
